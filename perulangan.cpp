8a86701da5a7408275221dff0f14478bd9f3233f
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-17 15:29:53 +0700

    menambahkan cout nilai n di naik

--- perulangan.cpp
+++ perulangan.cpp
@@ -13,4 +13,9 @@ int main()
 
     cout << "Perulangan for pencacah naik" << endl;
     for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
 }
