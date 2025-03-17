3dc705ac744fbf1c29c24047aa53ccfcc1dc70b4
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-17 15:45:15 +0700

    menambahkan while

--- perulangan.cpp
+++ perulangan.cpp
@@ -35,5 +35,22 @@ int main()
     n = rand() % 10;
 
     cout << "Nilai awal n = " << n << endl;
+
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
+        n = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
+    cout "Perulangan do while" << endl;
+
+    srand(time(0));
+
+    n = rand () % 10;
+
+    cout << "Nilai awal n = " << n << endl;
     
 }
