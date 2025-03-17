cfb037fd456f42d759a71f032b5c21de61ae75b4
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-17 15:24:41 +0700

    perulangan naik

--- perulangan.cpp
+++ perulangan.cpp
@@ -2,3 +2,15 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    //Perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+}
