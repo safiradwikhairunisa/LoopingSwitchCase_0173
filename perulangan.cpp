0143b86afbd247bda4adba386ffaff7ad26b4d87
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-03-17 15:33:29 +0700

    perulangan for turun

--- perulangan.cpp
+++ perulangan.cpp
@@ -18,4 +18,14 @@ int main()
     }
     cout << "Nilai n terakhir = " << n << endl;
     cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
+    // Perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+    
 }
